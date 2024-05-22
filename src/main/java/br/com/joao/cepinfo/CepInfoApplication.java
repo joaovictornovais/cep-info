393bd647f8ef88623c7e5e0c17292db3a5fd11c5
João Victor Novais <joao.dipaay@gmail.com>
2024-05-22 13:07:45 -0300

    initial config

---
+++ src/main/java/br/com/joao/cepinfo/CepInfoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.cepinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joao/cepinfo/CepInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.cepinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
